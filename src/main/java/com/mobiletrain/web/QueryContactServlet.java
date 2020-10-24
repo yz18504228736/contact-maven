3c599e3a0478e14818c540e8d9ca129ebe3af5b5
yang <18504228736@163.com>
2020-10-24 17:02:35 +0800

    第一次提交

---
+++ src/main/java/com/mobiletrain/web/QueryContactServlet.java
@@ -0,0 +1,33 @@
+package com.mobiletrain.web;
+
+import com.mobiletrain.domain.Contact;
+import com.mobiletrain.service.ContactService;
+import com.mobiletrain.service.impl.ContactServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/query_contact")
+public class QueryContactServlet extends HttpServlet {
+    private ContactService service = new ContactServiceImpl();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // 1. 接收数据
+        // 2. 处理数据
+        List<Contact> contacts = service.queryAll();
+
+        // 3. 响应数据
+        request.setAttribute("contacts", contacts);
+        request.getRequestDispatcher("/list.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
